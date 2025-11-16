d6685945e25cbe22b3cf7d9361466302a3950795
aayushtmG <aiyoustmg@gmail.com>
2025-11-16 16:55:34 +0545

    updated

--- handler_validate.go
+++ handler_validate.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	// "fmt"
 	"net/http"
+	"strings"
 )
 
 
@@ -11,7 +13,7 @@ func handleChirpsValidate(w http.ResponseWriter,r *http.Request){
 			Body string `json:"body"`
 		}
 		type returnVals struct {
-			Valid bool `json:"valid"`
+			CleanedBody string `json:"cleaned_body"`
 		}
 
 		decoder := json.NewDecoder(r.Body)
@@ -26,9 +28,37 @@ func handleChirpsValidate(w http.ResponseWriter,r *http.Request){
 			respondWithError(w,http.StatusBadRequest,"Chirp is too long",nil)
 			return
 		}
-
+		badWords := map[string]struct{}{
+			"kerfuffle": {},
+			"sharbert": {},
+			"fornax": {},
+		}
+		cleaned := getCleanedBody(params.Body,badWords)
 		respondWithJson(w,200,returnVals{
-			Valid: true,
+			CleanedBody: cleaned,
 		})
 
+}
+
+//this was my solution
+// func filterBadWord(s string) string{
+// 	splited := strings.Split(s," ")
+// 	for in, str := range splited{
+// 		l := strings.ToLower(str)
+// 		if l == "kerfuffle" || l == "sharbert" ||  l == "fornax" {
+// 			splited[in] = "****"
+// 		}
+// 	}
+// 	return strings.Join(splited, " ")
+// }
+func getCleanedBody(body string, badWords map[string]struct{}) string {
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		loweredWord := strings.ToLower(word)
+		if _, ok := badWords[loweredWord]; ok {
+			words[i] = "****"
+		}
+	}
+	cleaned := strings.Join(words, " ")
+	return cleaned
 }
